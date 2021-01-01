2bd3367730107c741274a7390e0acea828a780fd
didik <dhs0223@gmail.com>
2021-01-01 16:57:25 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::match(['GET', 'POST'], 'login', 'UserController@login');
+Route::match(['GET', 'POST'], 'register', 'UserController@register');
+
+Route::get('dashboard', 'DashboardController@page');
+Route::get('profile', 'DashboardController@profile');
+Route::get('stock', 'DashboardController@stock');
